8a052725877dee47c51379e430ee45365b47f2db
Imre <rabl.imre95@gmail.com>
2024-10-28 19:56:24 +0100

    getting familiar with components, props, states, conditional rendering, useeffect

---
+++ src/components/Text.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import './Text.css'
+
+function Text({content, color}) {
+    // console.log(props);
+
+    // const {content, color} = props;
+
+    // const content = props.content;
+    // const color = props.color;
+
+    const [counter, setCounter] = useState(0);
+    console.log(content,counter)
+
+    const handleClick = () => setCounter((currentValue) => currentValue + 1)
+
+    return (
+        <>
+        <h2 className={color}>{content}</h2>
+        <p>counter: {counter}</p>
+        <button onClick={handleClick} disabled={counter >= 10 ? true : false}>increase</button>
+        {counter >= 10 && <span>enough!</span>}
+        </>
+    )
+}
+
+export default Text;
